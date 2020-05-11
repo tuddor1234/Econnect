a2c2dea984b66d3d544b20789ddc4a9cd127b361
Prejban Mircea <46164263+MirciuPirciu@users.noreply.github.com>
2020-05-11 21:52:49 +0300

    Update admin.py

--- backend/src/econnect/admin.py
+++ backend/src/econnect/admin.py
@@ -3,8 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Test
+from .models import User
+from .models import Training
+from .models import Trainer
+from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Test)
+admin.site.register(User)
+admin.site.register(Trainer)
+admin.site.register(Training)
+admin.site.register(Profile)
